f1bea2501a10563b6e76b859182b8bc93cb07cec
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-08-14 10:43:11 +0800

    function with arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 pub fn print() {
-    let numbers = vec![1, 2, 3, 4, 5];
-    for n in numbers {
+    let numbers = [1, 2, 3, 4, 5];
+    output_sequence(numbers);
+}
+
+fn output_sequence(numbers: [u8; 5]) {
+    for n in numbers.iter() {
         println!("{}", n);
     }
 }
