64072e028b3762450e1eada7b159cb523693f389
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-08-14 10:41:31 +0800

    use std::vec::Vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub fn print() {
-    let numbers: [u8; 5] = [1, 2, 3, 4, 5];
-    for n in numbers.iter() {
+    let numbers = vec![1, 2, 3, 4, 5];
+    for n in numbers {
         println!("{}", n);
     }
 }
