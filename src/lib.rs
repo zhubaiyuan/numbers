7dc0da87182966163acfa7e99c395e00ee6d69c9
zhubaiyuan <21332201+zhubaiyuan@users.noreply.github.com>
2019-08-14 10:45:35 +0800

    passing the right type of the argument to a function

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 pub fn print() {
-    let numbers = [1, 2, 3, 4, 5];
+    let numbers = vec![1, 2, 3, 4, 5];
     output_sequence(numbers);
 }
 
-fn output_sequence(numbers: [u8; 5]) {
-    for n in numbers.iter() {
+fn output_sequence(numbers: Vec<u8>) {
+    for n in numbers {
         println!("{}", n);
     }
 }
